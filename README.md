3e81315c8a0f6513be0b777659abb853128bacf8
Araf Farayez <araf.farayez@gmail.com>
2024-02-16 22:21:20 +0600

    add google social authentication

--- README.md
+++ README.md
@@ -1,3 +1,58 @@
 # Sample Node.js application
 
 This repository is a sample Node.js application for Docker's documentation.
+
+Created using guidelines from https://docs.docker.com/language/nodejs/
+
+# Essential commands
+- Build image and start application
+    ```
+    docker compose up --build
+    ```
+- Detatched mode
+    ```
+    docker compose up -d --build
+    ```
+- Enter container terminal
+    ```
+    docker compose exec server sh
+    ```
+- Enter container terminal as root user
+    ```
+    docker compose exec -u root server sh
+    ```
+- Stop and remove containers
+    ```
+    docker compose down
+    ```
+- Run tests
+    ```
+    docker compose run server npm run test
+    ```
+- Build new image using test stage and run tests
+    ```
+    docker build -t nodejs-base-server --progress=plain --no-cache --target test .
+    ```
+- Docker CLI access
+    ```
+    docker login -u farayez
+    dckr_pat_JnryyvcbHA-Y3Gd2dBWhdULUnvc
+    ```
+
+## Local Kubernetes deployment
+- deploy to local kubernetes
+    ```
+    kubectl apply -f docker-node-kubernetes.yaml
+    ```
+- List deployments
+    ```
+    kubectl get deployments
+    ```
+- List services
+    ```
+    kubectl get services
+    ```
+- Tear down application
+    ```
+    kubectl delete -f docker-node-kubernetes.yaml
+    ```
--- src/index.js
+++ src/index.js
@@ -5,25 +5,91 @@ const getItems = require('./routes/getItems');
 const addItem = require('./routes/addItem');
 const updateItem = require('./routes/updateItem');
 const deleteItem = require('./routes/deleteItem');
+const path = require('path');
 
+// Configure .env file parser
+const dotenv = require('dotenv');
+dotenv.config();
+
+// Configure Express and EJS
 app.use(express.json());
 app.use(express.static(__dirname + '/static'));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
+// Configure Session
+const session = require('express-session');
+app.use(session({
+    resave: false,
+    saveUninitialized: true,
+    secret: 'SECRET'
+}));
+
+// Define routes
 app.get('/items', getItems);
 app.post('/items', addItem);
 app.put('/items/:id', updateItem);
 app.delete('/items/:id', deleteItem);
+app.get('/auth', function (req, res) {
+    res.render('pages/auth');
+});
 
+/*  PASSPORT SETUP  */
+
+const passport = require('passport');
+var userProfile;
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/success', (req, res) => res.render('pages/success', { user: userProfile }));
+app.get('/error', (req, res) => res.send("error logging in"));
+
+passport.serializeUser(function (user, cb) {
+    cb(null, user);
+});
+
+passport.deserializeUser(function (obj, cb) {
+    cb(null, obj);
+});
+
+/*  Google AUTH  */
+
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/callback"
+},
+    function (accessToken, refreshToken, profile, done) {
+        userProfile = profile;
+        return done(null, userProfile);
+    }
+));
+
+
+app.get('/auth/google',
+    passport.authenticate('google', { scope: ['profile', 'email'] }));
+
+app.get('/auth/google/callback',
+    passport.authenticate('google', { failureRedirect: '/error' }),
+    function (req, res) {
+        // Successful authentication, redirect success.
+        res.redirect('/success');
+    });
+
+// Initialize application
 db.init().then(() => {
-    app.listen(3000, () => console.log('Listening on port 3000'));
+    const port = process.env.PORT || 3000;
+    app.listen(port, () => console.log('Listening on port ' + port));
 }).catch((err) => {
     console.error(err);
     process.exit(1);
 });
 
+// Teardown application
 const gracefulShutdown = () => {
     db.teardown()
-        .catch(() => {})
+        .catch(() => { })
         .then(() => process.exit());
 };
 
