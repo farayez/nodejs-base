f45ea87548ee863c97e80c706ab88041aca0592d
Araf Farayez <araf.farayez@gmail.com>
2024-02-19 00:01:20 +0600

    temporarily fix persistance

--- src/persistence/index.js
+++ src/persistence/index.js
@@ -1,2 +1,10 @@
-if (process.env.POSTGRES_HOST) module.exports = require('./postgres');
-else module.exports = require('./sqlite');
+// if (process.env.POSTGRES_HOST) module.exports = require('./postgres');
+// else module.exports = require('./sqlite');
+
+// import postgres from './postgres.js';
+import sqlite from './sqlite.js';
+// let db;
+// if (process.env.POSTGRES_HOST) db = sqlite;
+// else db = sqlite;
+
+export default sqlite;
