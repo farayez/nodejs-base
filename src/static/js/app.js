bb16575693d6b0614334fa14c05cf038a392755e
Araf Farayez <araf.farayez@gmail.com>
2024-02-21 13:25:13 +0600

    fix static demo

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -15,21 +15,21 @@ function TodoListCard() {
     const [items, setItems] = React.useState(null);
 
     React.useEffect(() => {
-        fetch('/items')
-            .then(r => r.json())
+        fetch('api/items')
+            .then((r) => r.json())
             .then(setItems);
     }, []);
 
     const onNewItem = React.useCallback(
-        newItem => {
+        (newItem) => {
             setItems([...items, newItem]);
         },
         [items],
     );
 
     const onItemUpdate = React.useCallback(
-        item => {
-            const index = items.findIndex(i => i.id === item.id);
+        (item) => {
+            const index = items.findIndex((i) => i.id === item.id);
             setItems([
                 ...items.slice(0, index),
                 item,
@@ -40,8 +40,8 @@ function TodoListCard() {
     );
 
     const onItemRemoval = React.useCallback(
-        item => {
-            const index = items.findIndex(i => i.id === item.id);
+        (item) => {
+            const index = items.findIndex((i) => i.id === item.id);
             setItems([...items.slice(0, index), ...items.slice(index + 1)]);
         },
         [items],
@@ -55,7 +55,7 @@ function TodoListCard() {
             {items.length === 0 && (
                 <p className="text-center">No items yet! Add one above!</p>
             )}
-            {items.map(item => (
+            {items.map((item) => (
                 <ItemDisplay
                     item={item}
                     key={item.id}
@@ -73,16 +73,16 @@ function AddItemForm({ onNewItem }) {
     const [newItem, setNewItem] = React.useState('');
     const [submitting, setSubmitting] = React.useState(false);
 
-    const submitNewItem = e => {
+    const submitNewItem = (e) => {
         e.preventDefault();
         setSubmitting(true);
-        fetch('/items', {
+        fetch('api/items', {
             method: 'POST',
             body: JSON.stringify({ name: newItem }),
             headers: { 'Content-Type': 'application/json' },
         })
-            .then(r => r.json())
-            .then(item => {
+            .then((r) => r.json())
+            .then((item) => {
                 onNewItem(item);
                 setSubmitting(false);
                 setNewItem('');
@@ -94,7 +94,7 @@ function AddItemForm({ onNewItem }) {
             <InputGroup className="mb-3">
                 <Form.Control
                     value={newItem}
-                    onChange={e => setNewItem(e.target.value)}
+                    onChange={(e) => setNewItem(e.target.value)}
                     type="text"
                     placeholder="New Item"
                     aria-describedby="basic-addon1"
@@ -118,7 +118,7 @@ function ItemDisplay({ item, onItemUpdate, onItemRemoval }) {
     const { Container, Row, Col, Button } = ReactBootstrap;
 
     const toggleCompletion = () => {
-        fetch(`/items/${item.id}`, {
+        fetch(`api/items/${item.id}`, {
             method: 'PUT',
             body: JSON.stringify({
                 name: item.name,
@@ -126,12 +126,12 @@ function ItemDisplay({ item, onItemUpdate, onItemRemoval }) {
             }),
             headers: { 'Content-Type': 'application/json' },
         })
-            .then(r => r.json())
+            .then((r) => r.json())
             .then(onItemUpdate);
     };
 
     const removeItem = () => {
-        fetch(`/items/${item.id}`, { method: 'DELETE' }).then(() =>
+        fetch(`api/items/${item.id}`, { method: 'DELETE' }).then(() =>
             onItemRemoval(item),
         );
     };
