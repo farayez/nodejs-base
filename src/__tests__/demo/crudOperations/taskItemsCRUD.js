40a8fecc099f229f25bbfa7946ea6f6a1b172b4c
Araf Farayez <araf.farayez@gmail.com>
2024-02-24 02:11:38 +0600

    add demo CRUD tests with live model

---
+++ src/__tests__/demo/crudOperations/taskItemsCRUD.js
@@ -0,0 +1,36 @@
+import db from '#models';
+
+export async function getItems() {
+    return await db.TaskItem.findAll();
+}
+
+export async function getItem(id) {
+    return await db.TaskItem.findByPk(id);
+}
+
+export async function addItem(data) {
+    let item = db.TaskItem.build({
+        name: data.name,
+        longDescription: data.longDescription,
+    });
+    await item.save();
+
+    return item;
+}
+
+export async function udpateItem(id, data) {
+    const item = await db.TaskItem.findByPk(id);
+    item.completed = data.completed ?? item.completed;
+    item.name = data.name ?? item.name;
+    item.longDescription = data.longDescription ?? item.longDescription;
+
+    return await item.save();
+}
+
+export async function deleteItem(id) {
+    let item = await db.TaskItem.findByPk(id);
+
+    await item.destroy();
+
+    return true;
+}
---
+++ src/__tests__/demo/crudOperations/taskItemsCRUD.test.js
@@ -0,0 +1,70 @@
+import { syncDatabase } from '#utils/tests/setupTestDatabase.js';
+import {
+    getItem,
+    getItems,
+    addItem,
+    udpateItem,
+    deleteItem,
+} from './taskItemsCRUD.js';
+
+describe('it performs CRUD operations on model successfully', () => {
+    beforeEach(function () {
+        return syncDatabase();
+    });
+
+    afterEach(function () {
+        return syncDatabase();
+    });
+
+    const itemSample1 = {
+        id: 1,
+        name: 'sample 1',
+        longDescription: 'long des 1',
+        completed: false,
+    };
+
+    const itemSample2 = {
+        id: 1,
+        name: 'sample 2',
+        longDescription: 'long des 2',
+        completed: true,
+    };
+
+    it('performs Create and Read operation', async function () {
+        let result = await addItem(itemSample1);
+        expect(result).toMatchObject(itemSample1);
+
+        result = await getItem(1);
+        expect(result).toMatchObject(itemSample1);
+
+        result = await getItems();
+        expect(result).toHaveLength(1);
+        expect(result).toContainObject(itemSample1);
+    });
+
+    it('performs Create and Update operation', async function () {
+        let result = await addItem(itemSample1);
+        result = await getItems();
+        expect(result).toHaveLength(1);
+
+        result = await udpateItem(1, {
+            name: itemSample2.name,
+            longDescription: itemSample2.longDescription,
+            completed: itemSample2.completed,
+        });
+
+        expect(result).toMatchObject(itemSample2);
+
+        result = await getItem(1);
+        expect(result).toMatchObject(itemSample2);
+    });
+
+    it('performs Create and Delete operation', async function () {
+        let result = await addItem(itemSample1);
+        expect(await getItems()).toHaveLength(1);
+
+        result = await deleteItem(1);
+        expect(result).toEqual(true);
+        expect(await getItems()).toHaveLength(0);
+    });
+});
