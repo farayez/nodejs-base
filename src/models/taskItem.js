49e3887e99748bee87bb4461e8bb02c1c9d2241d
Araf Farayez <araf.farayez@gmail.com>
2024-02-24 01:54:38 +0600

    reorganize sequelize

--- src/models/taskItem.js
+++ src/models/taskItem.js
@@ -1,5 +1,5 @@
 import { DataTypes } from 'sequelize';
-import sequelize from './utils/sequelize.js';
+import sequelize from '#utils/sequelize.js';
 
 const TaskItem = sequelize.define(
     'TaskItem',
--- src/utils/sequelize.js
+++ src/utils/sequelize.js
@@ -4,6 +4,11 @@ const env = process.env.NODE_ENV || 'development';
 const config = databaseConfig[env];
 
 import { Sequelize } from 'sequelize';
+
+if (config.logging) {
+    console.log('database-config', config);
+}
+
 const sequelize = new Sequelize(
     config.database,
     config.username,
