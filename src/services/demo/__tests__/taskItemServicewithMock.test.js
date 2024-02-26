2b41e23511d429d30de1752fb6e7a6bf7e71e4b3
Araf Farayez <araf.farayez@gmail.com>
2024-02-27 01:29:06 +0600

    increase test coverage

--- src/services/demo/__tests__/taskItemServicewithMock.test.js
+++ src/services/demo/__tests__/taskItemServicewithMock.test.js
@@ -77,23 +77,47 @@ describe('it performs CRUD operations on mock model successfully', () => {
         jest.spyOn(modelsMock.TaskItem, 'findByPk').mockReturnValue(
             modelInstance,
         );
-        jest.spyOn(modelInstance, 'save').mockReturnValue(itemSample2);
+        jest.spyOn(modelInstance, 'save').mockReturnValue({
+            id: 1,
+            ...itemSample2,
+        });
 
         let result = await taskItemService.updateItem(2, {
             name: itemSample2.name,
             longDescription: itemSample2.longDescription,
             completed: itemSample2.completed,
         });
-        expect(modelsMock.TaskItem.findByPk.mock.calls.length).toBe(1);
-        expect(modelsMock.TaskItem.findByPk.mock.calls[0][0]).toEqual(2);
+        expect(modelsMock.TaskItem.findByPk).toHaveBeenCalledWith(2);
         expect(modelsMock.TaskItem.findByPk).toHaveBeenCalledBefore(
             modelInstance.save,
         );
         expect(modelInstance.save.mock.contexts[0]).toMatchObject(itemSample2);
-        expect(result).toMatchObject(itemSample2);
+        expect(result).toMatchObject({
+            id: 1,
+            ...itemSample2,
+        });
     });
 
-    it('updateItem works properly during partial update', async function () {});
+    it('updateItem works properly during partial update', async function () {
+        jest.spyOn(modelsMock.TaskItem, 'findByPk').mockReturnValue(
+            modelInstance,
+        );
+        jest.spyOn(modelInstance, 'save').mockReturnValue({
+            id: 1,
+            ...itemSample1,
+        });
+
+        let result = await taskItemService.updateItem(2, {});
+        expect(modelsMock.TaskItem.findByPk).toHaveBeenCalledWith(2);
+        expect(modelsMock.TaskItem.findByPk).toHaveBeenCalledBefore(
+            modelInstance.save,
+        );
+        expect(modelInstance.save.mock.contexts[0]).toMatchObject(itemSample1);
+        expect(result).toMatchObject({
+            id: 1,
+            ...itemSample1,
+        });
+    });
 
     it('performs Delete operation', async function () {
         jest.spyOn(modelsMock.TaskItem, 'findByPk').mockReturnValue(
