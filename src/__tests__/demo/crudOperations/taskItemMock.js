c259639abb2513889cdac2e163b03e45b9926385
Araf Farayez <araf.farayez@gmail.com>
2024-02-26 15:25:01 +0600

    refactor test, add delete CRUD operation test

--- src/__tests__/demo/crudOperations/taskItemMock.js
+++ src/__tests__/demo/crudOperations/taskItemMock.js
@@ -1,21 +1,12 @@
 import { jest } from '@jest/globals';
 
-// export const modelInstance = {
-//     name: 'sample 1',
-//     longDescription: 'long des 1',
-//     completed: false,
-//     save: jest.fn(),
-// };
-
 export default (modelInstance) =>
     jest.unstable_mockModule('#models', () => ({
         default: {
             TaskItem: {
-                findByPk: jest.fn(() => modelInstance),
+                findByPk: jest.fn(),
                 findAll: jest.fn(() => [modelInstance]),
                 build: jest.fn(() => modelInstance),
             },
         },
     }));
-
-// export default
--- src/__tests__/demo/crudOperations/taskItemsCRUDwithMock.test.js
+++ src/__tests__/demo/crudOperations/taskItemsCRUDwithMock.test.js
@@ -13,47 +13,61 @@ const itemSample2 = {
     completed: true,
 };
 
-var modelInstance = {
-    ...itemSample1,
+let modelFunctions = {
     save: jest.fn(),
+    destroy: jest.fn(),
+};
+
+let modelInstance = {
+    ...itemSample1,
+    ...modelFunctions,
 };
 
 taskItemMock(modelInstance);
 
 // Import modules after setting up mocks
-const { default: dbMock } = await import('#models');
+const { default: modelsMock } = await import('#models');
 const { getItem, getItems, addItem, udpateItem, deleteItem } = await import(
     './taskItemsCRUD.js'
 );
 
 describe('it performs CRUD operations on mock model successfully', () => {
-    afterEach(() => {
+    beforeEach(() => {
+        modelInstance = {
+            ...itemSample1,
+            ...modelFunctions,
+        };
         modelInstance.save.mockRestore();
     });
 
     it('performs Read one operation', async function () {
+        jest.spyOn(modelsMock.TaskItem, 'findByPk').mockReturnValue(
+            modelInstance,
+        );
+
         let result = await getItem(2);
-        expect(dbMock.TaskItem.findByPk.mock.calls.length).toBe(1);
-        expect(dbMock.TaskItem.findByPk.mock.calls[0][0]).toEqual(2);
+        expect(modelsMock.TaskItem.findByPk.mock.calls.length).toBe(1);
+        expect(modelsMock.TaskItem.findByPk.mock.calls[0][0]).toEqual(2);
         expect(result).toMatchObject(itemSample1);
     });
 
     it('performs Read all operation', async function () {
         let result = await getItems();
-        expect(dbMock.TaskItem.findAll.mock.calls.length).toBe(1);
+        expect(modelsMock.TaskItem.findAll.mock.calls.length).toBe(1);
         expect(result).toContainObject(itemSample1);
     });
 
     it('performs Create operation', async function () {
-        jest.spyOn(modelInstance, 'save').mockReturnValue(itemSample1);
+        jest.spyOn(modelInstance, 'save').mockReturnValue(modelInstance);
+
         let result = await addItem(itemSample1);
-        expect(dbMock.TaskItem.build.mock.calls.length).toBe(1);
-        expect(dbMock.TaskItem.build.mock.calls[0][0]).toMatchObject({
+        expect(modelsMock.TaskItem.build.mock.calls.length).toBe(1);
+        expect(modelsMock.TaskItem.build.mock.calls[0][0]).toMatchObject({
             name: itemSample1.name,
             longDescription: itemSample1.longDescription,
         });
         expect(modelInstance.save.mock.calls.length).toBe(1);
-        expect(dbMock.TaskItem.build).toHaveBeenCalledBefore(
+        expect(modelsMock.TaskItem.build).toHaveBeenCalledBefore(
             modelInstance.save,
         );
         expect(modelInstance.save.mock.contexts[0]).toMatchObject(itemSample1);
@@ -62,18 +76,41 @@ describe('it performs CRUD operations on mock model successfully', () => {
     });
 
     it('performs Update operation', async function () {
+        jest.spyOn(modelsMock.TaskItem, 'findByPk').mockReturnValue(
+            modelInstance,
+        );
         jest.spyOn(modelInstance, 'save').mockReturnValue(itemSample2);
+
         let result = await udpateItem(2, {
             name: itemSample2.name,
             longDescription: itemSample2.longDescription,
             completed: itemSample2.completed,
         });
-        expect(dbMock.TaskItem.findByPk.mock.calls.length).toBe(1);
-        expect(dbMock.TaskItem.findByPk.mock.calls[0][0]).toEqual(2);
-        expect(dbMock.TaskItem.findByPk).toHaveBeenCalledBefore(
+        expect(modelsMock.TaskItem.findByPk.mock.calls.length).toBe(1);
+        expect(modelsMock.TaskItem.findByPk.mock.calls[0][0]).toEqual(2);
+        expect(modelsMock.TaskItem.findByPk).toHaveBeenCalledBefore(
             modelInstance.save,
         );
         expect(modelInstance.save.mock.contexts[0]).toMatchObject(itemSample2);
         expect(result).toMatchObject(itemSample2);
+    });
+
+    it('performs Delete operation', async function () {
+        jest.spyOn(modelsMock.TaskItem, 'findByPk').mockReturnValue(
+            modelInstance,
+        );
+
+        let result = await deleteItem(2);
+        expect(modelsMock.TaskItem.findByPk.mock.calls.length).toBe(1);
+        expect(modelsMock.TaskItem.findByPk.mock.calls[0][0]).toEqual(2);
+        expect(modelsMock.TaskItem.findByPk).toHaveBeenCalledBefore(
+            modelInstance.destroy,
+        );
+
+        expect(modelInstance.destroy.mock.calls.length).toBe(1);
+        expect(modelInstance.destroy.mock.contexts[0]).toMatchObject(
+            itemSample1,
+        );
+        expect(result).toBe(true);
     });
 });
