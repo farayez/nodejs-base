20c9935fc1090eb57a114a4659064c984718f651
Araf Farayez <araf.farayez@gmail.com>
2024-03-06 00:14:57 +0600

    add tests for auth failure

--- src/__tests__/e2e/utils/validate-token.test.foreign.js
+++ src/__tests__/e2e/utils/validate-token.test.foreign.js
@@ -4,10 +4,26 @@ import auth from '#utils/tests/setupAuth.foreign.js';
 const request = supertest(app);
 
 describe('validate-token endpoints work correctly', function () {
-    it('gets all task items using get all endpoint', async () => {
+    it('should pass if token is valid', async () => {
         const response = await request
             .get('/api/validate-token')
             .set('Authorization', `Bearer ${auth.accessToken}`);
         expect(response.status).toBe(200);
+    });
+
+    it('should deny access if token is not present in header', async () => {
+        const response = await request.get('/api/validate-token');
+        expect(response.status).toBe(401);
+        expect(response.body).toMatchObject({
+            error: 'No authorization token was found',
+        });
+    });
+
+    it('should deny access if token is invalid', async () => {
+        const response = await request
+            .get('/api/validate-token')
+            .set('Authorization', `Bearer falseToken`);
+        expect(response.status).toBe(401);
+        expect(response.body).toMatchObject({ error: 'jwt malformed' });
     });
 });
